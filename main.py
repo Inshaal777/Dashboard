084f75669c65a64855a7495b693ea9dd19756d66
Inshal777 <inshalsikandar4@gmail.com>
2024-10-23 01:06:35 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+# create a Dash application
+app = dash.Dash(__name__)
+
+# define the layout of the Dashboard
+app.layout = html.Div(
+    children=[
+        html.H1("My Dashboard"),
+        dcc.Graph(
+            id="My-Graph",
+            figure={
+                "data": [
+                    {"x": [1,2,3],"y": [4,1,2], "type":"bar", "name":"Bar chart"},
+                    {"x": [1,2,3],"y": [2,4,5], "type":"line", "name":"Line chart"},
+                ],
+                "layout": {
+                    "title": "Graph title",
+                    "xaxis": {"title": "x-axis"},
+                    "yaxis": {"title": "y-axis"},
+                }
+            }
+        )
+    ]
+)
+
+# Run the application
+if __name__ == "__main__":
+    app.run_server(debug=True)
+    
